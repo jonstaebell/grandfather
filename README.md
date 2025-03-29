d69eb050a3439b6513cb954c3bdea87f156a004b
Jon Staebell <jonstaebell@gmail.com>
2025-03-29 18:02:59 -0500

    v2.0 initial commit, uses chrontab

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# grandfather
-Turns a Raspberry Pi and Google Home into a grandfather clock
+# torscan
+(c) 2025 Jon Staebell
+
+Program to turn a Google Chromecast into a grandfather clock
+
+## Description
+
+When run, makes a sound like a grandfather clock. Uses simple server to send .wav file from port 8000
+of computer to a specified Chromecast using Pychromecast.
+
+### Dependencies
+
+Requires crontab set up to run at 15/30/45 minutes during desired times. e.g.:
+0,15,30,45 * 7-21 * * /path/to/python /path/to/grandfather.py
+
+Set the following parameters in grandfather.ini: 
+   device_friendly_name
+   clockvolume
+   webhook_url (optional, set to "" to disable Discord webhook calls on errors)
+(if program is renamed, need to rename the .ini file. E.g. if renamed "foo.py" it looks for "foo.ini")
+
+Requires simple server running on port 8000 on same computer
+
+### Executing program
+
+* install in same directory as grandfather.ini
+* python3 grandfather.py 
+
+## Authors
+
+Jon Staebell
+jonstaebell@gmail.com
+
+## Version History
+
+* v2 rewrite to use chrontab, code cleanup 3/29/2025
+* V1
+    * Initial Release 2018
+
+## License
+
+torscan Copyright (C) 2025 Jon Staebell
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## Acknowledgments
+
+
+
--- grandfather.py
+++ grandfather.py
@@ -1,140 +1,97 @@
-#Grandfather clock application by Jon Staebell 1/4/2019
-#updated static IP address 12/16/2021
-#updated 12/18/20202, pychromecast doesn't support python 2 so
-# ran pip install PyChromecast==1.0.3 zeroconf==0.19.1
-# updated 2/27/2025 to handle Chromecast device offline
-
-import datetime, time, pychromecast, urllib.request, socket
+# Grandfather 2.0 (c) 2025 Jon Staebell
+#
+# NOTE! requires crontab set up to run at 15/30/45 minutes during desired times. e.g.:
+# 0,15,30,45 * 7-21 * * /path/to/python /path/to/grandfather.py
+#
+# Need to change the following parameters below:
+#   webhook_url (optional, set to "" to disable Discord webhook calls on errors
+#   device_friendly_name
+#   clockvolume
+#
+# Requires simple server running on port 8000 (see media parameter)
+#
+import time, pychromecast, os, requests, socket, configparser, sys
 from discord_webhook import DiscordWebhook
 
-# set url for discord webhook if Chromecast device offline
-webhook_url = "https://discord.com/api/webhooks/1297969513353973770/0_IoX6hluZvKUPLtRnQ9e1uQ8piP0IkCYbM7Fw3JCoupgjwFlKDCNJDdkmZ542FIyxZj"
+def main():
+    params = get_config(sys.argv[0].replace(".py", ".ini"))
+    webhook_url = params ['webhook_url']
+    device_friendly_name = params ['device_friendly_name']
+    clockvolume = params ['clockvolume']
+
+    # build filepath, including IP address, to send to simple server
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8",80))
+    media = 'http://' + s.getsockname()[0] + ':8000' + os.getcwd() + "/bong" + get_filename()  + ".wav"
+    cast = get_cast(device_friendly_name)
+    if cast  == "":
+        call_webhook(webhook_url, "Grandfather Missing Chromecast " + device_friendly_name) #error or missing chromecast
+    else:
+        call_cast(cast, media, webhook_url, clockvolume)
+
+def get_config(config_file):
+    # return paramaters from configuration file
+    param_dict = {}
+    try:
+        Config = configparser.ConfigParser()
+        Config.read(config_file)
+        param_dict['device_friendly_name'] = Config.get('required', 'device_friendly_name')
+        param_dict['clockvolume'] = float(Config.get('required', 'clockvolume'))
+        param_dict['webhook_url'] = Config.get('optional', 'webhook_url')
+    except:
+        print (f"invalid config file {config_file}")
+        sys.exit() # exit on exceptions
+    return param_dict
 
 
-# set beginning and ending hours for the clock chimes
-beginhour = 7
-endhour = 21
+def call_cast(cast, media, webhook_url, clockvolume):
+    # plays specified media on the specified chromecast
+    # clockvolume is volume at which to play the media
+    # calls optional webhook if there's an error playing the media
+    #
+    try:
+        cast.wait() # wait until ready
+        oldvolume = cast.status.volume_level # save current volume
+        cast.set_volume(clockvolume) # set volume to specified
+        mc = cast.media_controller
+        mc.play_media(media, content_type = 'audio/wav')
+        time.sleep(10) # allow to play
+        cast.set_volume(oldvolume) # change volume back to previous
+    except:
+        call_webhook(webhook_url, "Grandfather error playing media " + media)
 
-# Set volume for clock chimes between 0 and 1:
-clockvolume = .2
+def get_cast(device_friendly_name):
+    # returns the Chromecast specified. If pychromecast call errors or it's not found, returns ""
+    #
+    try:
+        chromecasts = pychromecast.get_chromecasts()
+    except:
+        return ""
 
-# display debug messages?
-debug = False
+    # check to see if the Chromecast is available
+    for cc in chromecasts:
+            if cc.device.friendly_name == device_friendly_name:
+                    return cc
+    return ""
 
-# Your Chromecast device Friendly Name
-device_friendly_name = "Living Room speaker"
 
-chromecasts = pychromecast.get_chromecasts()
+def call_webhook(webhook_url, output_message):
+    # checks the webhook_url parameter, and if present, uses it to send webhook to Discord
+    #
+    # if url provided and minute is "00" (to avoid spamming notifications), notify Discord to add alert
+    if webhook_url != "" and time.strftime("%M") == "00":
+        webhook = DiscordWebhook(url=webhook_url, content=f"{output_message}")
+        response = webhook.execute()
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print("Error in trying to use Discord Webhook", err)
 
-# select Chromecast device
-#cast = next(cc for cc in chromecasts if cc.device.friendly_name == device_friendly_name)
-device_present = False
-device_alarm = False
+def get_filename():
+    # Returns part of filename needed to specify the media file
+    # if the minute is "00", returns the hour ("1", "2", etc.), else returns the minute ("15", "30", or "45")
+    hour, minute = time.strftime("%I %M").split(" ")
+    return hour if minute == "00" else minute
 
-cast = ""
-
-# check to see if the Chromecast is available
-
-while not device_present:
-        for cc in chromecasts:
-                if cc.device.friendly_name == device_friendly_name:
-                        device_present = True
-
-        if not device_present:
-                if not device_alarm:
-                        message = device_friendly_name + " is not running, grandfather paused"
-                        webhook = DiscordWebhook(url=webhook_url, content=message)
-                        response = webhook.execute()
-                        device_alarm = True
-                time.sleep(60*5) # sleep for 5 minutes
-
-cast = next(cc for cc in chromecasts if cc.device.friendly_name == device_friendly_name)
-
-if debug:
-        print (device_present, cast)
-        _ = input("press any key to continue")
-
-time.sleep (0.5)
-
-# wait for the device
-cast.wait()
-if debug:
-        print(cast.device)
-        print(cast.status)
-        print ("starting")
-
-# get media controller
-mc = cast.media_controller
-# need to make sure simplehttpserver is running to serve local audio file
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8",80))
-filepath = 'http://' + s.getsockname()[0] + ':8000/home/pi/grandfather/'
-if debug: print(filepath)
-#filepath = 'http://192.168.2.150:8000/home/pi/grandfather/'
-
-bing15file = filepath + 'bing15.wav'
-bing30file = filepath + 'bing30.wav'
-bing45file = filepath + 'bing45.wav'
-
-# function to cast file to Chromcast device
-def castclock(clockfile):
-        oldvolume = cast.status.volume_level
-        cast.set_volume(clockvolume)
-        if debug: print ("set volume to ", clockvolume)
-        time.sleep(1)
-
-        if debug: print(cast.status)
-        if debug: print ('playing ',clockfile)
-        mc.play_media(clockfile, content_type = 'audio/wav')
-
-        time.sleep(60)
-
-        cast.set_volume(oldvolume)
-        if debug: print ("reset volume to ", oldvolume)
-        time.sleep(10)
-
-# debug chromecast
-if debug:
-        bongfile = filepath + 'bong12.wav'
-        print ('Starting debug')
-        castclock(bongfile)
-        print ("end of debug")
-
-while True:
-        if debug: print ("in loop")
-        currentDT = datetime.datetime.now()
-        hour = currentDT.hour
-        hour12 = hour%12
-        if hour12 == 0:
-                hour12 = 12
-        minute = currentDT.minute
-        second = currentDT.second
-
-        if hour >= beginhour and hour <= endhour:
-
-                if minute == 0:
-                        if debug: print (hour, ":",minute,":",second)
-                        bongfile = filepath + 'bong' + str(hour12) + '.wav'
-                        castclock(bongfile)
-
-                if minute == 15:
-                        if debug:
-                                print (hour, ":",minute,":",second)
-                                print("Bing15")
-                        castclock(bing15file)
-
-                if minute == 30:
-                        if debug:
-                                print (hour, ":",minute,":",second)
-                                print ("Bing30")
-                        castclock(bing30file)
-
-                if minute == 45:
-                        if debug:
-                                print (hour, ":",minute,":",second)
-                                print ("Bing45")
-                        castclock(bing45file)
-
-        time.sleep(10)
-
-# end of code
+if __name__ == "__main__":
+    main()
